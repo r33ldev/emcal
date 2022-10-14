f48a0e614e6c1c156b59c0285a5082c4a5db30b1
Michael E Adebisi <adebisienads@gmail.com>
2022-10-14 15:59:06 +0100

    daily view

---
+++ src/components/DailyView/DailyCalendar.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+import { setCurrentView } from "../../redux/appslice";
+import { getDayData } from "../../utils/utility";
+import Time from "../WeekView/Time";
+import Week from "../WeekView/Week";
+
+const DailyCalendar = () => {
+  const dispatch = useDispatch();
+  function handleBack() {
+    dispatch(setCurrentView("Monthly"));
+  }
+  const dayData = getDayData();
+  return (
+    <div style={{ height: "calc(100vh-34px)" }}>
+      <button onClick={handleBack} className='mt-6'>
+        <span className='material-icons-outlined cursor-pointer text-gray-600 mx-2'>
+          <span className='material-symbols-outlined'>arrow_back</span>
+        </span>
+      </button>
+      {dayData.map((day, idx) => {
+        const dayToShow = [day]
+        if (idx === 0) return <Week currentWeek={dayToShow} />;
+        return <Time currentWeek={day} index={idx} />;
+      })}
+    </div>
+  );
+};
+
+export default DailyCalendar;
